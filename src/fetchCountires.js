bd025fad0d7ef63077957b0f1e07dfc0b14f8822
YuDo-fsd <y.doloban@gmail.com>
2023-04-02 16:51:06 +0200

    last

--- src/fetchCountires.js
+++ src/fetchCountires.js
@@ -1,15 +1,21 @@
-const BASE_URL = 'https://restcountries.com/v2';
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const options = ['name', 'capital', 'population', 'flags', 'languages'];
+const searchParams = new URLSearchParams({
+  fields: options.join(','),
+});
 
-function fetchCountries(name) {
-  const url = `${BASE_URL}/name/${name}?fields=name,flags,capital,population,languages`;
-
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
-    return response.json();
-  });
+export default function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?${searchParams}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(response =>
+      response.map(item => ({
+        name: item['name']['official'],
+        capital: item['capital'],
+        population: item['population'],
+        flags: item['flags']['svg'],
+        languages: item['languages'],
+      }))
+    );
 }
-
-export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,95 +1,88 @@
 import './css/styles.css';
-import { fetchCountries } from '../src/fetchCountires';
+import fetchCountries from './fetchCountires';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfoContainer = document.querySelector('.country-info-container');
-
-const renderCountryList = countries => {
-  countryList.innerHTML = '';
-
-  countries.forEach(country => {
-    const { name, flags } = country;
-    const flagImg = document.createElement('img');
-    flagImg.src = flags.svg;
-    flagImg.alt = `${name.official} flag`;
-    const countryName = document.createElement('span');
-    countryName.textContent = name.official;
-    const listItem = document.createElement('li');
-    listItem.append(flagImg, countryName);
-    countryList.append(listItem);
-
-    listItem.addEventListener('click', () => {
-      renderCountryInfo(country);
-    });
-  });
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-const renderCountryInfo = country => {
-  const {
-    name: { official },
-    capital,
-    population,
-    flags,
-    languages,
-  } = country;
-
-  const languagesArr = Object.values(languages)
-    .map(language => language.name)
-    .join(', ');
-
-  const countryInfo = `
-      <div class="country-info">
-        <img src="${flags.svg}" alt="${official} flag" />
-        <h2>${official}</h2>
-        <p><strong>Capital:</strong> ${capital}</p>
-        <p><strong>Population:</strong> ${population}</p>
-        <p><strong>Languages:</strong> ${languagesArr}</p>
-      </div>
-    `;
-
-  countryInfoContainer.innerHTML = countryInfo;
+const listItemTemp = ({ name, flags }) => {
+  return `
+        <li class="country-list__item">
+          <img class="country-list__image" src="${flags}" alt="Country flag of ${name}" width="30" height="30" />
+          <p class="country-list__offname">${name}</p>
+        </li>
+      `;
 };
 
-const searchCountries = event => {
-  const searchQuery = event.target.value.trim();
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-  if (!searchQuery) {
-    countryList.innerHTML = '';
-    countryInfoContainer.innerHTML = '';
+function onSearch(e) {
+  const inputValue = e.target.value.trim();
+
+  removeChildren(refs.countryList);
+  removeChildren(refs.countryInfo);
+
+  if (inputValue === '') {
     return;
   }
 
-  fetchCountries(searchQuery)
+  function removeChildren(e) {
+    while (e.firstChild) {
+      e.removeChild(e.firstChild);
+    }
+  }
+
+  const data = fetchCountries(inputValue);
+
+  data
     .then(countries => {
       if (countries.length > 10) {
-        Notiflix.Notify.info(
+        Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
-      } else if (countries.length > 1 && countries.length <= 10) {
-        renderCountryList(countries);
-        countryInfoContainer.innerHTML = '';
-      } else if (countries.length === 1) {
-        renderCountryInfo(countries[0]);
-        countryList.innerHTML = '';
-      } else {
-        Notiflix.Notify.failure(
-          'Oops! No country found. Please enter a valid name.'
-        );
-        countryList.innerHTML = '';
-        countryInfoContainer.innerHTML = '';
+        return;
       }
+
+      if (countries.length === 1) {
+        renderCard(countries[0]);
+        return;
+      }
+
+      renderList(countries);
     })
     .catch(error => {
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong. Please try again later.'
-      );
-      console.error(error);
+      Notify.failure('Oops, there is no country with that name');
     });
-};
+}
 
-searchBox.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+function renderList(countries) {
+  refs.countryList.innerHTML = countries
+    .map(({ name, flags }) => listItemTemp({ name, flags }))
+    .join('');
+}
+
+function renderCard(country) {
+  const { name, capital, population, flags, languages } = country;
+  const cardOfCountry = `
+    <p class="country-info__field">
+      <img class="country-info__img" src="${flags}" alt="Country flag of ${name}" width="40" height="40"/>
+      ${name}
+    </p>
+    <p class="country-info__field">
+      <b>Capital:</b> ${capital.join(', ')}
+    </p>
+    <p class="country-info__field">
+      <b>Population:</b> ${population}
+    </p>
+    <p class="country-info__field">
+      <b>Languages:</b> ${Object.values(languages).join(', ')}
+    </p>
+  `;
+  refs.countryInfo.innerHTML = cardOfCountry;
+}
